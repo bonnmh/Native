e054c4d5390e23c8073fa9257188bce92bf9030d
bonnmh <nmhbonc2thptcd@gmail.com>
2021-12-20 23:22:52 +0700

    rename android

--- android/app/src/main/java/com/bonnmh/fortune/MainActivity.java
+++ android/app/src/main/java/com/bonnmh/fortune/MainActivity.java
@@ -1,4 +1,4 @@
-package com.native;
+package com.bonnmh.fortune;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Native";
+    return "Fortune";
   }
 }
